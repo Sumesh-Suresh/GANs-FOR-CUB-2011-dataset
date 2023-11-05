ca0457071e4e96e0f9d6516289b97c89d787a26d
Sumesh K S <115145152+Sumesh-Suresh@users.noreply.github.com>
2023-11-04 22:53:13 -0400

    Update and rename q1_5.py to wgan.py

--- wgan.py
+++ wgan.py
@@ -13,15 +13,15 @@ def compute_discriminator_loss(
     discrim_real, discrim_fake, discrim_interp, interp, lamb
 ):
     """
-    TODO 1.5.1: Implement WGAN-GP loss for discriminator.
+    Implementing WGAN-GP loss for discriminator.
     loss = E[D(fake_data)] - E[D(real_data)] + lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
     """
-    ##################################################################
-    # TODO 1.5: Implement WGAN-GP loss for discriminator.
+   
+    # WGAN-GP loss for discriminator.
     # loss_pt1 = E[D(fake_data)] - E[D(real_data)]
     # loss_pt2 = lambda * E[(|| grad wrt interpolated_data (D(interpolated_data))|| - 1)^2]
     # loss = loss_pt1 + loss_pt2
-    ##################################################################
+  
     loss_pt1 = torch.mean(discrim_fake) - torch.mean(discrim_real)
     gradients = torch.autograd.grad(outputs=discrim_interp, 
                                     inputs=interp, 
@@ -32,21 +32,17 @@ def compute_discriminator_loss(
                                 )[0]
     loss_pt2 = lamb * torch.mean((torch.linalg.norm(gradients, dim=1, ord=2) - 1) ** 2)
     loss = loss_pt1 + loss_pt2
-    ##################################################################
-    #                          END OF YOUR CODE                      #
-    ##################################################################
+
     return loss
 
 
 def compute_generator_loss(discrim_fake):
-    ##################################################################
-    # TODO 1.5: Implement WGAN-GP loss for generator.
+   
+    # Implement WGAN-GP loss for generator.
     # loss = - E[D(fake_data)]
-    ##################################################################
+    
     loss = -1*torch.mean(discrim_fake)
-    ##################################################################
-    #                          END OF YOUR CODE                      #
-    ##################################################################
+    
     return loss
 
 
