efb4da19c074705751bac3c7622413f9e41f2294
Sumesh K S <115145152+Sumesh-Suresh@users.noreply.github.com>
2023-11-04 22:50:09 -0400

    Update vanilla_gan.py

--- vanilla_gan.py
+++ vanilla_gan.py
@@ -14,11 +14,7 @@ from train import train_model
 def compute_discriminator_loss(
     discrim_real, discrim_fake, discrim_interp, interp, lamb
 ):
-    ##################################################################
-    # TODO 1.3: Implement GAN loss for discriminator.
-    # Do not use discrim_interp, interp, lamb. They are placeholders
-    # for Q1.5.
-    ##################################################################
+   
     criterion = torch.nn.functional.binary_cross_entropy_with_logits
     
     real_labels = torch.ones_like(discrim_real)
@@ -26,35 +22,23 @@ def compute_discriminator_loss(
     loss_real = criterion(discrim_real, real_labels)
     loss_fake = criterion(discrim_fake, fake_labels)
     loss = loss_real + loss_fake
-    ##################################################################
-    #                          END OF YOUR CODE                      #
-    ##################################################################
+   
     return loss
 
 
 def compute_generator_loss(discrim_fake):
-    ##################################################################
-    # TODO 1.3: Implement GAN loss for the generator.
-    ##################################################################
+   
     criterion = torch.nn.functional.binary_cross_entropy_with_logits
     real_labels = torch.ones_like(discrim_fake)  
     loss = criterion(discrim_fake, real_labels)
     return loss
-    ##################################################################
-    #                          END OF YOUR CODE                      #
-    ##################################################################
-    
-
+   
 
 if __name__ == "__main__":
     args = get_args()
     gen = Generator().to(device)
     disc = Discriminator().to(device)
-    # print(gen)
-    # print('\n\n')
-    # print(disc)
-    # print('\n\n')
-    # exit()
+   
     prefix = "data_gan/"
     os.makedirs(prefix, exist_ok=True)
 
